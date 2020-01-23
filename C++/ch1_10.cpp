975e8710b4227a9d33f8203fb36d7a29a7f68ef3
sonsuhyune <s3267s@naver.com>
2020-01-23 16:56:47 +0900

    C++의 using을이용한 namespace

---
+++ C++/ch1_10.cpp
@@ -0,0 +1,14 @@
+//using namespace
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+//이후부는 cin, cout, endl --> 이렇게쓸 수 있다.
+
+int main(void) {
+	int num = 20;
+	cout << "Hello" << endl;
+	cout << num << " " << "A" << endl;
+	return 0;
+}
